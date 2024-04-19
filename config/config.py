dc2a07e2c70f6c686b2eec8e3f17b208ff688a7b
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-04-19 19:44:48 +0330

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ _C.model = CN(new_allowed=True)
 _C.dataset = CN(new_allowed=True)
 
 _C.train.device = 'cuda'
-_C.train.num_workers = 8
+_C.train.num_workers = 2
 
 # Choose the type of the model
 _C.model.type = 'SpeechFormer'   # Transformer, SpeechFormer, SpeechFormer++/SpeechFormer_v2
@@ -29,9 +29,9 @@ _C.train.save_best = False
 _C.train.folds = None    # modify in the main process
 
 # Select a database to train a model
-_C.dataset.database = 'iemocap'   # iemocap, meld, pitt, daic_woz
+_C.dataset.database = 'daic_woz'   # iemocap, meld, pitt, daic_woz
 # Select a kind of feature to train a model
-_C.dataset.feature = 'wav2vec'    # spec, wav2vec, logmel, hubert12, hubert24, w2v12, w2v24
+_C.dataset.feature = 'spec'    # spec, wav2vec, logmel, hubert12, hubert24, w2v12, w2v24
 # Select a padding mode when preparing data a mini-batch 
 _C.dataset.padmode = 'constant'   # constant, repeat
 
