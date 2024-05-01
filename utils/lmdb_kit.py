35b35148201ee7fff9b22cf2f9ff8d0f9e8bf067
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-05-01 22:22:50 +0330

    Update lmdb_kit.py

--- utils/lmdb_kit.py
+++ utils/lmdb_kit.py
@@ -151,7 +151,8 @@ if __name__ == '__main__':
         'database': 'daic_woz',
         'feature': 'spec',
         # 'feature': 'wavlm24',
-        'lmdb_name': 'daic_woz_wavlm_L24',
+        # 'lmdb_name': 'daic_woz_wavlm_L24',
+        'lmdb_name': 'daic_woz_spec_v6',
         'lmdb_root': '/148Dataset/data-chen.weidong/lmdb',
         'commit_interval': 100,
         'state': 'train'   # Valid when database is meld or daic_woz.
